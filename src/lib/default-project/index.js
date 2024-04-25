07ca8136f038261625d63772507bb90d50bf4797
Aness6040 <aness2805@gmail.com>
2024-04-25 14:55:46 +0200

    Update index.js

--- src/lib/default-project/index.js
+++ src/lib/default-project/index.js
@@ -1,29 +1,14 @@
 import projectData from './project-data';
 
 /* eslint-disable import/no-unresolved */
-import overrideDefaultProject from '!arraybuffer-loader!./default-project.sb3';
 import backdrop from '!raw-loader!./cd21514d0531fdffb22204e0ec5ed84a.svg';
 import costume1 from '!raw-loader!./f02dc12ef367ece5d75fd23937cfc9e6.svg';
 /* eslint-enable import/no-unresolved */
-import {TextEncoder} from '../tw-text-encoder';
+import { TextEncoder } from '../tw-text-encoder';
 
 const defaultProject = translator => {
-    if (overrideDefaultProject.byteLength > 0) {
-        return [{
-            id: 0,
-            assetType: 'Project',
-            dataFormat: 'JSON',
-            data: overrideDefaultProject
-        }];
-    }
+    const encoder = new TextEncoder();
 
-    let _TextEncoder;
-    if (typeof TextEncoder === 'undefined') {
-        _TextEncoder = require('text-encoding').TextEncoder;
-    } else {
-        _TextEncoder = TextEncoder;
-    }
-    const encoder = new _TextEncoder();
     const projectJson = projectData(translator);
     return [{
         id: 0,
@@ -36,14 +21,11 @@ const defaultProject = translator => {
         dataFormat: 'SVG',
         data: encoder.encode(backdrop)
     }, {
-<<<<<<< HEAD
         id: 'f02dc12ef367ece5d75fd23937cfc9e6',
-=======
-        id: 'c434b674f2da18ba13cdfe51dbc05ecc',
->>>>>>> a08c6df361c3cf42c4a5cc1a0234afff72896efa
         assetType: 'ImageVector',
         dataFormat: 'SVG',
         data: encoder.encode(costume1)
     }];
 };
+
 export default defaultProject;
